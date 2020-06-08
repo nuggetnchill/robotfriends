713814f9f1a73cd5c25ff10af5812588783949a1
nuggetnchill <nuggetnchill@gmail.com>
2020-06-08 15:44:20 -0500

    Updates

---
+++ precache-manifest.27e021375d643fba49de44c6046012e9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6bb87dd7aa1fd463bb97fab381987abf",
+    "url": "/robotfriends/index.html"
+  },
+  {
+    "revision": "21e56a1ed608e9205018",
+    "url": "/robotfriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "e9f570646a02c6cfb102",
+    "url": "/robotfriends/static/css/main.c98d12a3.chunk.css"
+  },
+  {
+    "revision": "21e56a1ed608e9205018",
+    "url": "/robotfriends/static/js/2.ee828747.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robotfriends/static/js/2.ee828747.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e9f570646a02c6cfb102",
+    "url": "/robotfriends/static/js/main.0f31fb0c.chunk.js"
+  },
+  {
+    "revision": "e854fbeaa6fd8c233fd4",
+    "url": "/robotfriends/static/js/runtime-main.db5d5516.js"
+  },
+  {
+    "revision": "86c545ccae4a76a95a9ffdd312221e69",
+    "url": "/robotfriends/static/media/MR ROBOT.86c545cc.woff"
+  }
+]);
